c60a6d45fd46bf3792299c09b050df68cbc0b905
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-05 20:14:57 +0700

    Create the project

---
+++ SnapCarouselSlider/SnapCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCarouselSliderApp.swift
+//  SnapCarouselSlider
+//
+//  Created by Danh Tu on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SnapCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
